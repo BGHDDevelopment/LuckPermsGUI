7eeededb43bb882c06fb62b735ef0ebe1326ef3f
StephanBH <bghdpayments@gmail.com>
2019-11-25 13:44:44 -0800

    Initial commit

---
+++ src/me/AsVaidas/LuckPemsGUI/users/Permissions.java
@@ -0,0 +1,215 @@
+package me.AsVaidas.LuckPemsGUI.users;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+import me.AsVaidas.LuckPemsGUI.Main;
+import me.AsVaidas.LuckPemsGUI.Tools;
+import me.lucko.luckperms.LuckPerms;
+import me.lucko.luckperms.api.Contexts;
+import me.lucko.luckperms.api.LuckPermsApi;
+import me.lucko.luckperms.api.Node;
+import me.lucko.luckperms.api.User;
+
+public class Permissions implements Listener {
+
+	public static Map<Player, User> addPermission = new HashMap<Player, User>();
+	public static Map<Player, User> addTempPermission = new HashMap<Player, User>();
+	public static Map<Player, User> checkIfHas = new HashMap<Player, User>();
+	
+	@EventHandler
+	public void onPermissionAdd(AsyncPlayerChatEvent e) {
+		if (!addPermission.containsKey(e.getPlayer())) return;
+		String message = e.getMessage();
+		User g = addPermission.get(e.getPlayer());
+		
+		Tools.sendCommand(e.getPlayer(), "lp user "+g.getName()+" permission set "+message);
+		addPermission.remove(e.getPlayer());
+		Bukkit.getScheduler().scheduleSyncDelayedTask(Main.getInstance(), () -> {
+			EditUser.open(e.getPlayer(), g);
+		});
+		e.setCancelled(true);
+	}
+	
+	@EventHandler
+	public void onTempPermissionAdd(AsyncPlayerChatEvent e) {
+		if (!addTempPermission.containsKey(e.getPlayer())) return;
+		String message = e.getMessage();
+		User g = addTempPermission.get(e.getPlayer());
+		
+		Tools.sendCommand(e.getPlayer(), "lp user "+g.getName()+" permission settemp "+message);
+		addTempPermission.remove(e.getPlayer());
+		Bukkit.getScheduler().scheduleSyncDelayedTask(Main.getInstance(), () -> {
+			EditUser.open(e.getPlayer(), g);
+		});
+		e.setCancelled(true);
+	}
+	
+	@EventHandler
+	public void onPermissionCheck(AsyncPlayerChatEvent e) {
+		if (!checkIfHas.containsKey(e.getPlayer())) return;
+		String message = e.getMessage();
+		User g = checkIfHas.get(e.getPlayer());
+		
+		Tools.sendCommand(e.getPlayer(), "lp user "+g.getName()+" permission check "+message);
+		checkIfHas.remove(e.getPlayer());
+		Bukkit.getScheduler().scheduleSyncDelayedTask(Main.getInstance(), () -> {
+			EditUser.open(e.getPlayer(), g);
+		});
+		e.setCancelled(true);
+	}
+	
+	static LuckPermsApi l = LuckPerms.getApi();
+
+	public static void open(Player p, User user, int page) {
+		Inventory myInventory = Bukkit.createInventory(null, 54, ChatColor.AQUA+"LuckPerms user permissions");
+		Tools.onAsync(() -> {
+
+		// ----------------------- INFO ------------------------------
+		ItemStack info = Tools.button(Material.ARMOR_STAND,
+				"&6Info",
+				Arrays.asList(
+						"&cName: &e"+user.getName(),
+						"&cUUID: &e"+user.getUuid(),
+						"&cGroup: &e"+user.getPrimaryGroup(),
+						"&cCounts:",
+						"&c   Nodes: &e"+user.getNodes().size(),
+						"&c   Permissions: &e"+user.getPermissions().size(),
+						"&c   Prefixes: &e"+user.getCachedData().getMetaData(Contexts.global()).getPrefixes().size(),
+						"&c   Suffixes: &e"+user.getCachedData().getMetaData(Contexts.global()).getSuffixes().size(),
+						"&cCached data:",
+						"&c   Current prefix: &e"+user.getCachedData().getMetaData(Contexts.global()).getPrefix(),
+						"&c   Current suffix: &e"+user.getCachedData().getMetaData(Contexts.global()).getSuffix()
+						),
+				1);
+		myInventory.setItem(4, info);
+		// ----------------------- INFO ------------------------------
+		
+		int sk = 0;
+		int i = 9;
+		
+		int from = 45*page-1;
+		int to = 45*(page+1)-1;
+		for (Node permission : user.getPermissions()) {
+			if (permission.isGroupNode()) continue;
+			if (permission.isPrefix()) continue;
+			if (permission.isSuffix()) continue;
+			if (permission.isMeta()) continue;
+			if (permission.getPermission().contains("weight")) continue;
+			if (from <= sk && sk < to) {
+				String expiration;
+				try {
+					expiration = Tools.getTime(permission.getExpiry().getTime());
+				} catch (Exception e) {
+					expiration = "Never";
+				}
+				
+				String server;
+				try {
+					server = permission.getServer().get();
+				} catch (Exception e) {
+					server = "global";
+				}
+				
+				String world;
+				try {
+					world = permission.getWorld().get();
+				} catch (Exception e) {
+					world = "global";
+				}
+				
+				ItemStack item = Tools.button(Material.TNT,
+						"&6"+permission.getPermission(),
+						Arrays.asList(
+								"&cID: &e"+sk,
+								"&cExpires in: &e"+expiration,
+								"&cValue: &e"+permission.getValue(),
+								"&cServer: &e"+server,
+								"&cWorld: &e"+world,
+								"&eClick to remove"
+								), 1);
+				myInventory.setItem(i, item);
+				i++;
+			}
+			sk++;
+		}
+
+		if (to < sk) {
+			ItemStack next = Tools.button(Material.SIGN, "&6Next", Arrays.asList("&eNext page", "&cCurrent: &e"+page), 1);
+			myInventory.setItem(53, next);
+		}
+		
+		ItemStack back = Tools.button(Material.BARRIER, "&6Back", Arrays.asList(""), 1);
+		myInventory.setItem(8, back);
+		
+		});
+		p.openInventory(myInventory);
+	}
+	
+	@EventHandler
+	public void onInventoryClickEvent(InventoryClickEvent e) {
+		Player p = (Player) e.getWhoClicked();
+
+		Inventory inv = e.getClickedInventory();
+		ItemStack item = e.getCurrentItem();
+		if (inv != null && item != null)
+			if (e.getView().getTitle().equals(ChatColor.AQUA+"LuckPerms user permissions")) {
+				e.setCancelled(true);
+				if (item.hasItemMeta())
+					if (item.getItemMeta().hasDisplayName()) {
+						
+						String group = ChatColor.stripColor(inv.getItem(4).getItemMeta().getLore().get(0).split(" ")[1]);
+						User g = LuckPerms.getApi().getUser(group);
+						
+						String name = ChatColor.stripColor(item.getItemMeta().getDisplayName());
+						if (name.equals("Next")) {
+							int current = Integer.valueOf(ChatColor.stripColor(inv.getItem(53).getItemMeta().getLore().get(1).split(" ")[1]));
+							open(p, g, current+1);
+						} else if (name.equals("Back")) {
+							EditUser.open(p, g);
+						} else if (!name.equals("Info")) {
+							
+							int id = Integer.valueOf(ChatColor.stripColor(item.getItemMeta().getLore().get(0).split(" ")[1]));
+
+							int sk = 0;
+							for (Node permission : g.getPermissions()) {
+								if (permission.isGroupNode()) continue;
+								if (permission.isPrefix()) continue;
+								if (permission.isSuffix()) continue;
+								if (permission.isMeta()) continue;
+								if (permission.getPermission().contains("weight")) continue;
+								if (sk == id) {
+									if (permission.isTemporary())
+										Tools.sendCommand(p, "lp user "+g.getName()+" unsettemp "+'"'+permission.getPermission()+'"'+" "+Tools.contextConverter(permission.getFullContexts()));
+									else
+										Tools.sendCommand(p, "lp user "+g.getName()+" unset "+'"'+permission.getPermission()+'"'+" "+Tools.contextConverter(permission.getFullContexts()));
+									break;
+								}
+								sk++;
+							}
+								
+							int current = 0;
+							if (inv.getItem(53) != null)
+								current = Integer.valueOf(ChatColor.stripColor(inv.getItem(53).getItemMeta().getLore().get(1).split(" ")[1]));
+
+							int page = current;
+							Bukkit.getScheduler().runTaskLater(Main.getInstance(), () -> {
+								open(p, g, page);
+							}, 3);
+						}
+					}
+			}
+	}
+}
---
+++ src/me/AsVaidas/LuckPemsGUI/users/UsersGUI.java
@@ -0,0 +1,96 @@
+package me.AsVaidas.LuckPemsGUI.users;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+import me.AsVaidas.LuckPemsGUI.Main;
+import me.AsVaidas.LuckPemsGUI.Tools;
+import me.lucko.luckperms.LuckPerms;
+import me.lucko.luckperms.api.LuckPermsApi;
+
+public class UsersGUI implements Listener {
+
+	static LuckPermsApi l = LuckPerms.getApi();
+	
+	List<Player> editUser = new ArrayList<Player>();
+
+	@EventHandler
+	public void onUserEdit(AsyncPlayerChatEvent e) {
+		if (!editUser.contains(e.getPlayer())) return;
+		String message = e.getMessage();
+		editUser.remove(e.getPlayer());
+
+		Bukkit.getScheduler().scheduleSyncDelayedTask(Main.getInstance(), () -> {
+			EditUser.open(e.getPlayer(), message);
+		});
+			
+		e.setCancelled(true);
+	}
+	
+	public static void open(Player p) {
+		Inventory myInventory = Bukkit.createInventory(null, 54, ChatColor.AQUA+"LuckPerms users");
+		Tools.onAsync(() -> {
+			
+			int sk = 9;
+			for (Player player : Bukkit.getOnlinePlayers()) {
+				if (sk >= 53) continue;
+				ItemStack item = Tools.head(player.getName(),
+						"&6"+player.getName(), 
+						Arrays.asList("&eClick to edit"), 1);
+						
+				myInventory.setItem(sk, item);
+				sk++;
+			}
+
+			ItemStack newGroup = Tools.button(Material.PAPER, "&6Edit offline user", Arrays.asList("&eClick to edit offline/online user"), 1);
+			myInventory.setItem(0, newGroup);
+
+			ItemStack wipe = Tools.button(Material.REDSTONE, "&4Remove ranks", Arrays.asList("&eRemove from all users", "&especific ranks.", "&e(Great for wipes)"), 1);
+			myInventory.setItem(1, wipe);
+
+			ItemStack back = Tools.button(Material.BARRIER, "&6Back", Arrays.asList(""), 1);
+			myInventory.setItem(8, back);
+			
+		});
+		p.openInventory(myInventory);
+	}
+	
+	@EventHandler
+	public void onInventoryClickEvent(InventoryClickEvent e) {
+		Player p = (Player) e.getWhoClicked();
+
+		Inventory inv = e.getClickedInventory();
+		ItemStack item = e.getCurrentItem();
+		if (inv != null && item != null)
+			if (e.getView().getTitle().equals(ChatColor.AQUA+"LuckPerms users")) {
+				e.setCancelled(true);
+				if (item.hasItemMeta())
+					if (item.getItemMeta().hasDisplayName()) {
+						String name = ChatColor.stripColor(item.getItemMeta().getDisplayName());
+						if (name.equals("Back")) {
+							Main.openGUI(p);
+						} else if (name.equals("Edit offline user")) {
+							Tools.sendMessage(p, "&eWrite in chat:");
+							Tools.sendMessage(p, "&8<&7Name&8>");
+							Tools.sendMessage(p, "&aName - User name");
+							editUser.add(p);
+							p.closeInventory();
+						} else if (name.equals("Remove ranks")) {
+							Wipe.open(p);
+						} else EditUser.open(p, LuckPerms.getApi().getUser(name));
+					}
+			}
+	}
+}
